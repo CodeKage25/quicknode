f8984f4601e435ac5b6f61ade75e8a930e34c30a
Babatunde Abdulkareem <babzchizzy27@gmail.com>
2022-10-20 15:35:14 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+    return <Layout />
 }
 
 export default App;
